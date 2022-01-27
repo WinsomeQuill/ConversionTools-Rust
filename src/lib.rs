8449b71ccf1c224dd3fd46a5426b2a6f33cbfe2f
WinsomeQuill <Artemqa.belyakov07@gmail.com>
2022-01-27 15:13:21 +0700

    Functions return changed from "Value" to "Result"

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,9 @@
 // GitHub: https://github.com/WinsomeQuill/ConversionTools-Rust
 
 pub mod api {
-    use std::{io, collections::HashMap, fs::File};
+    use std::{collections::HashMap, fs::File};
     use reqwest;
+    use reqwest::Error;
     use reqwest::blocking::multipart::Form;
     use reqwest::blocking::{Client, multipart, Response};
     use serde_json::{Value, from_str, json};
@@ -16,59 +17,58 @@ pub mod api {
             Api { token: token, url: url }
         }
 
-        pub fn get_tasks(&self) -> Value {
+        pub fn get_tasks(&self) -> Result<Value, Error> {
             let url: String = format!("{}/tasks", &self.url);
             let client: Client = Client::new();
     
-            let resp: Value = match client 
+            let resp: Result<Value, Error> = match client 
                 .get(&url)
                 .bearer_auth(&self.token)
                 .header("Content-Type", "application/json")
                 .header("User-Agent", "conversion_tools_rust_reqwest")
                 .send() {
-                    Ok(response) => from_str(&response.text().unwrap()).unwrap(),
-                    Err(_) => json!({}),
+                    Ok(response) => Ok(from_str(&response.text().unwrap()).unwrap()),
+                    Err(e) => Err(e),
                 };
             resp
         }
 
-        pub fn get_task(&self, task_id: &str) -> Value {
+        pub fn get_task(&self, task_id: &str) -> Result<Value, Error> {
             let url: String = format!("{}/tasks/{}", &self.url, &task_id);
             let client: Client = Client::new();
     
-            let resp: Value = match client
+            let resp: Result<Value, Error> = match client
                 .get(&url)
                 .bearer_auth(&self.token)
                 .header("Content-Type", "application/json")
                 .header("User-Agent", "conversion_tools_rust_reqwest")
                 .send() {
-                    Ok(response) => from_str(&response.text().unwrap()).unwrap(),
-                    Err(_) => json!({}),
+                    Ok(response) => Ok(from_str(&response.text().unwrap()).unwrap()),
+                    Err(e) => Err(e),
                 };
             resp
         }
 
-        pub fn upload_file(&self, path: &str) -> Value {
+        pub fn upload_file(&self, path: &str) -> Result<Value, Error> {
             let url: String = format!("{}/files", &self.url);
             let form: Form = multipart::Form::new()
                 .file("file=@", &path).unwrap();
-    
                 
             let client: Client = Client::new();
-            let res: Value = match client
+            let res: Result<Value, Error> = match client
                 .post(&url)
                 .bearer_auth(&self.token)
                 .header("Content-Type", "multipart/form-data")
                 .header("User-Agent", "conversion_tools_rust_reqwest")
                 .multipart(form)
                 .send() {
-                    Ok(response) => from_str(&response.text().unwrap()).unwrap(),
-                    Err(_) => json!({}),
+                    Ok(response) => Ok(from_str(&response.text().unwrap()).unwrap()),
+                    Err(e) => Err(e),
                 };
             res
         }
 
-        pub fn create_task(&self, type_conv: &str, file_id: &str, args: &HashMap<&str, &str>) -> Value {
+        pub fn create_task(&self, type_conv: &str, file_id: &str, args: &HashMap<&str, &str>) -> Result<Value, Error> {
             let mut json_task: serde_json::Value = json!({});
     
             json_task["type"] = Value::String(type_conv.to_string());
@@ -86,33 +86,38 @@ pub mod api {
             let url: String = format!("{}/tasks", &self.url);
             let client: Client = Client::new();
     
-            let resp: Value = match client
+            let resp: Result<Value, Error> = match client
                 .post(&url)
                 .bearer_auth(&self.token)
                 .header("Content-Type", "application/json")
                 .header("User-Agent", "conversion_tools_rust_reqwest")
                 .json(&json_task)
                 .send() {
-                    Ok(response) => from_str(&response.text().unwrap()).unwrap(),
-                    Err(_) => json!({}),
+                    Ok(response) => Ok(from_str(&response.text().unwrap()).unwrap()),
+                    Err(e) => Err(e),
                 };
             resp
         }
 
-        pub fn download_file(&self, file_id: &str, output_path: &str) {
+        pub fn download_file(&self, file_id: &str, output_path: &str) -> Result<(), std::io::Error> {
             let url: String = format!("{}/files/{}", &self.url, &file_id);
             let client: Client = Client::new();
     
-            let mut resp: Response = client
+            let resp: Result<Response, Error> = match client
                 .get(&url)
                 .bearer_auth(&self.token)
                 .header("Content-Type", "application/json")
                 .header("User-Agent", "conversion_tools_rust_reqwest")
-                .send()
-                .unwrap();
-    
+                .send() {
+                    Ok(response) => Ok(response),
+                    Err(e) => Err(e),
+                };
+
             let mut out: File = File::create(&output_path).unwrap();
-            io::copy(&mut resp, &mut out).unwrap();
+            match std::io::copy(&mut resp.unwrap(), &mut out) {
+                Ok(_) => Ok(()),
+                Err(e) => Err(e),
+            }
         }
     }
 
